280038c1d67101934274f98eb8ba307ddf50b11d
Syed Rizwan <rizwansm@outlook.com>
2025-05-07 17:51:24 +0600

    made nodes for agentic flow

--- tmp/8.chatbot_langgraph.py
+++ tmp/8.chatbot_langgraph.py
@@ -15,6 +15,10 @@ from langchain.tools.retriever import create_retriever_tool
 from langgraph.graph import MessagesState
 from langchain.chat_models import init_chat_model
 
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode
+from langgraph.prebuilt import tools_condition
+
 from pydantic import BaseModel, Field
 from typing import Literal
 from langchain_core.messages import convert_to_messages
@@ -227,3 +231,54 @@ input = {
 
 response = generate_answer(input)
 response["messages"][-1].pretty_print()
+
+
+workflow = StateGraph(MessagesState)
+
+# Define the nodes we will cycle between
+workflow.add_node(generate_query_or_respond)
+workflow.add_node("retrieve", ToolNode([retriever_tool]))
+workflow.add_node(rewrite_question)
+workflow.add_node(generate_answer)
+
+workflow.add_edge(START, "generate_query_or_respond")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "generate_query_or_respond",
+    # Assess LLM decision (call `retriever_tool` tool or respond to the user)
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+
+# Edges taken after the `action` node is called.
+workflow.add_conditional_edges(
+    "retrieve",
+    # Assess agent decision
+    grade_documents,
+)
+workflow.add_edge("generate_answer", END)
+workflow.add_edge("rewrite_question", "generate_query_or_respond")
+
+# Compile
+graph = workflow.compile()
+
+
+for chunk in graph.stream(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "What does Lilian Weng say about types of reward hacking?",
+            }
+        ]
+    }
+):
+    for node, update in chunk.items():
+        print("Update from node", node)
+        update["messages"][-1].pretty_print()
+        print("\n\n")
